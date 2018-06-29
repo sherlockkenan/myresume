591964657504354634ac439c05d7fc91784ca0b1
keke <keke@keke-5.ipads-lab.se.sjtu.edu.cn>
2018-06-29 15:58:56 +0800

    update master

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 hackmyresume BUILD ./resume-jrs.json TO ./index.html -t ./node_modules/jsonresume-theme-onepage
-#hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./#node_modules/jsonresume-theme-onepage
+hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-onepage
 
